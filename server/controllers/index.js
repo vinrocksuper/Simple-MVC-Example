bef2ea71841bf1afdbf4408b53b24e3620e46fd9
Vincent Li (RIT Student) <vl5604@rit.edu>
2022-10-21 13:50:35 -0400

    Create, Read, Update finsihed. Need to do Delete (CRUD)

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -20,10 +20,19 @@ const hostIndex = (req, res) => {
 };
 
 const hostPage1 = async (req, res) => {
-  const docs = await Cat.find({}).lean().exec();
+  try {
+    const docs = await Cat.find({}).lean().exec();
+    return res.render('page1', { cats: docs });
+  }
+  catch (err) {
+    console.log(err);
+    return res.status(500);
+  }
+
 };
 
 const hostPage2 = (req, res) => {
+
   res.render('page2');
 };
 
@@ -35,21 +44,67 @@ const getName = (req, res) => {
 
 };
 
-const setName = (req, res) => {
+const setName = async (req, res) => {
   if (!req.body.firstname || !req.body.lastname || !req.body.beds) {
     return res.status(400).json({ error: 'firstname,lastname and beds are all required' });
   }
-  
-};
+  const catData = {
+    name: `${req.body.firstname} ${req.body.lastname}`,
+    bedsOwned: req.body.beds
+  }
 
-const searchName = (req, res) => {
-  if (!req.query.name) {
-    return res.status(400).json({ error: 'Name is required to perform a search' });
+  const newCat = new Cat(catData);
+  try {
+    await newCat.save();
+    lastAdded = newCat;
+    return res.json({
+      name: lastAdded.name,
+      beds: lastAdded.bedsOwned,
+    })
+  } catch (e) {
+    console.log(e);
+    return res.status(500).json({ error: 'failed to create cat' });
   }
 };
 
+const searchName = async (req, res) => {
+  if (!req.query.name) {
+    return res.status(400).json({ error: 'Name is required to perform a search' });
+  }
+
+  let doc;
+
+  const query = {
+    name: req.query.name // can directly pass this into findOne() as well
+  }
+
+  try {
+    doc = await Cat.findOne(query).select('name bedsOwned').exec();
+  } catch (e) {
+    console.log(e);
+    return res.status(500).json({ error: 'something went wrong' })
+  }
+
+  if (!doc) {
+    return res.json({ message: "No cat found" });
+  }
+
+  return res.json({ name: doc.name, beds: doc.bedsOwned });
+};
+
 const updateLast = (req, res) => {
-	
+  // console.log(lastAdded);
+
+  lastAdded.bedsOwned++;
+
+  lastAdded.save().then(() => {
+    return res.json({
+      name: lastAdded.name,
+      beds: lastAdded.bedsOwned,
+    })
+  }).catch(err => {
+    return res.status(500).json({ message: 'could not update last added cat' });
+  });
 };
 
 const notFound = (req, res) => {
